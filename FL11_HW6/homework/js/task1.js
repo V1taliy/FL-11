25ecc4745519551f8ea06871cf1843b32977b21a
Vitaliy Sakhno <vitaliy.sakhno1@gmail.com>
2019-07-07 13:33:25 +0300

    add home work 6

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,19 @@
-// Your code goes here
+                let a1 = prompt('enter didgit a1');
+                let a2 = prompt('enter didgit a2');
+                let b1 = prompt('enter didgit b1');
+                let b2 = prompt('enter didgit b2');
+                let c1 = prompt('enter midpoint c1');
+                let c2 = prompt('enter midpoint c2');
+
+                let myMidpoint, a, b, c, d, i=2;
+                if (a1 === a2 && b1 === b2 && c1 === c2) {
+                  a = parseInt(a1, 10);
+                  b = parseInt(b1, 10);
+                  c = parseInt(c1, 10);
+                  myMidpoint = (a + b)/i;
+                  d = myMidpoint === c ? 'true' : 'false';
+                 console.log(d);
+                } else {
+                  console.log('false');
+                }
+                
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,14 @@
 // Your code goes here
+let a = prompt('enter 1st digit');
+let b = prompt('enter 2st digit');
+let c = prompt('enter 3st digit');
+
+if(a <= 0 || b <= 0 || c <= 0) {
+    console.log('Triangle doesn’t exist');
+} else if (a === b && b === c && c === a) {
+console.log('Eequivalent triangle');
+} else if (a === b || b === c || c === a) {
+    console.log('Isosceles triangle!!!');
+} else {
+    console.log('Normal triangle');
+}
